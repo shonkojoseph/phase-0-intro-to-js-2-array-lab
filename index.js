32743bb0c642baf77efecc7a2153953a747fd6f3
Joseph <josephshonko99@gmail.com>
2024-04-02 21:04:04 +0300

    commiting array labs

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield",]
+
+function destructivelyAppendCat (){
+    cats.push("Ralph")
+}
+const destructivelyPrependCat=()=>{
+    cats.unshift('Bob')
+}
+const destructivelyRemoveLastCat=()=>{
+    cats.pop()
+}
+
+const destructivelyRemoveFirstCat=()=> {
+cats.shift()
+}
+
+const appendCat=()=>{
+    return['Milo','Otis', 'Garfield', 'Broom']
+}
+
+function prependCat(){
+    return['Arnold','Milo','Otis','Garfield']
+}
+
+function removeLastCat(){
+    return cats. slice(0,-1)
+}
+
+const removeFirstCat=()=>{
+    return cats.slice(1)
+}
